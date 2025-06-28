861540592aa938fe44e98aa238a05e769e22a51a
NATALI16-CREATOR <natymubelgat@gmail.com>
2025-06-28 15:32:50 -0600

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,14 @@
+function toggleFooter() {
+    const content = document.getElementById('footerContent');
+    const toggleBtn = document.querySelector('.footer-toggle i');
+    const isOpen = content.style.display === 'flex';
+
+    content.style.display = isOpen ? 'none' : 'flex';
+    toggleBtn.className = isOpen ? 'fas fa-chevron-up' : 'fas fa-chevron-down';
+  }
+    const btnMenu = document.getElementById('btnMenu');
+  const navMenu = document.getElementById('navMenu');
+
+  btnMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
